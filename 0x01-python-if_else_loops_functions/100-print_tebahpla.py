6a6ce91a20d26f5cafca645d94036171b3a467b3
etokmfoniso <etokmfoniso@yahoo.com>
2022-06-29 16:28:34 -0700

    task 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(122, 196, -1):
+for i in range(122, 96, -1):
     if i % 2 == 1:
         i = i - 32
-    print("{}".format(i), end=""
+    print("{:d}".format(i), end="")
