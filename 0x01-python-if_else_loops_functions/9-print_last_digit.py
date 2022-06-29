8a5d314a1c105de050b7638101962a7c27f24175
etokmfoniso <etokmfoniso@yahoo.com>
2022-06-28 17:11:22 -0700

    task nine

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    if number < 0:
-        last_digit = number % -10
+    if number == 0:
+        last_digit = 0
+        return last_digit
+    elif number < 0:
+        last_digit = number % -(10)
         return last_digit
     else:
         last_digit = number % 10
