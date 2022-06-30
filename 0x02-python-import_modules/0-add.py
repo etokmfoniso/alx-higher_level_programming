2d01bf14c46d1b163501d331c0114b17738a436c
etokmfoniso <etokmfoniso@yahoo.com>
2022-06-30 14:16:08 -0700

    task zero

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from add_0 import add(a, b)
+    a = 1
+    b = 2
+    sum_ab = add(1, 5)
+    print("{:d} + {:d} = {:d}".format(a,b,sum_ab), end="")
