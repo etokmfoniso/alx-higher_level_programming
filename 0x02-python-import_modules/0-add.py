f34d15b1b1f539c5b9069b39e96d8bfa524e0776
etokmfoniso <etokmfoniso@yahoo.com>
2022-06-30 14:18:26 -0700

    task zero

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     a = 1
     b = 2
     sum_ab = add(1, 5)
-    print("{:d} + {:d} = {:d}".format(a,b,sum_ab), end="")
+    print("{:d} + {:d} = {:d}".format(a,b,sum_ab))
