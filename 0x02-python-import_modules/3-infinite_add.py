50a76aeeb8d6c760987035779f48d59485085d84
etokmfoniso <etokmfoniso@yahoo.com>
2022-06-30 17:11:00 -0700

    task three

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+def system():
+    summ = 0
+    if len(argv) - 1 == 0:
+        print("{:d}".format(summ))
+    else:
+        for i in range(1, len(argv)):
+            summ += (int)(argv[i])
+        print("{:d}".format(summ))
+
+
+if__name__ == "__main__":
+    system()
