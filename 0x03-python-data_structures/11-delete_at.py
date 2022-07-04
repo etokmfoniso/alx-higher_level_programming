083b4bd274174d13554df3d1dd7577bc8699059d
etokmfoniso <etokmfoniso@yahoo.com>
2022-07-04 14:47:27 -0700

    task twelve

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    new_list = []
+    for item in my_list:
+        my_list[idx] = item
+        if idx < 0 or idx > len(my_list) - 1
+            return my_list
+        else:
+            if
