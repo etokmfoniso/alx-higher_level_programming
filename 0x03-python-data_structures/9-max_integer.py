8301855434fdc37f9b4a4ee1d3d8bae536f7c96c
etokmfoniso <etokmfoniso@yahoo.com>
2022-07-04 12:15:20 -0700

    task nine

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    highest = 0
+    if my_list == []:
+        highest = None
+    else:
+        for num in my_list:
+            if num > highest:
+                highest = num
+            else:
+                highest = highest
+        return highest
