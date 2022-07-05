88fb1014f2f298766e31760cd0b2be9d4c5b31d8
etokmfoniso <etokmfoniso@yahoo.com>
2022-07-05 07:28:42 -0700

    task eight update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence == " ":
-        return tuple(0, None)
+    if sentence == "":
+        return (0, None)
     else:
         return tuple((len(sentence), sentence[0]))
