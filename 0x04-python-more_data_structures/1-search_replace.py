15e5acb004baf88f1a4250df2ab10c32897d490d
etokmfoniso <etokmfoniso@yahoo.com>
2022-07-08 06:06:43 -0700

    task one

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    if search in new_list:
-        search = replace
+    for i in range(len(new_list) - 1):
+        if new_list[i] == search:
+            new_list[i] = replace
+        else:
+            new_list[i] = new_list[i]
     return new_list
-        
