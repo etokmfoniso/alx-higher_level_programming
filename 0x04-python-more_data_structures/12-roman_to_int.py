fe4b596dae06eeba2383c0b2bebc312f7e063fa3
etokmfoniso <etokmfoniso@yahoo.com>
2022-07-08 13:53:25 -0700

    task twelve

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -22,4 +22,4 @@ def roman_to_int(roman_string):
             num += roman_dict[roman_string[i]] * -1
         else:
             num += roman_dict[roman_string[i]]
-            return (num)
+    return (num)
