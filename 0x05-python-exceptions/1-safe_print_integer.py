8989de5fd4258a36200905d55792e91aa31202aa
etokmfoniso <etokmfoniso@yahoo.com>
2022-07-19 03:11:38 -0700

    task one

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -11,7 +11,8 @@ def safe_print_integer(value):
     """
     try:
         int(value)
-        print("{:d}".format()(value))
+        print("{:d}".format(value))
         return True
     except (TypeError, ValueError):
         return False
+    print("")
