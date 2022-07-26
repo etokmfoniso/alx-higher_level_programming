c5449720515ee8610dca2cce4d1a67476c296e13
etokmfoniso <etokmfoniso@yahoo.com>
2022-07-26 02:42:20 -0700

    real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""Defines a Rectangle"""
+
 class Rectangle:
     """ Real definition of a triangle """
+
     def __init__(self, width = 0, height = 0):
         """ initializes a triangle with private attributes """
+
         self.width = width
         self.height = height
 
@@ -12,12 +16,11 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        try:
-            self.__width = int(value)
-        except TypeError:
-            print("width must be an integer")
-        except ValueError:
-            print("width must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__vwidth = value
 
     @property
     def height(self):
@@ -25,9 +28,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        try:
-            self.__height = int(value)
-        except TypeError:
-            print("height must be an integer")
-        except ValueError:
-            print("height must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
